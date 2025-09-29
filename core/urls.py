6fa1242424ec986c9a0ab1f13801a0f8f1838af7
MicaelRiboura <micaelribouradossantos@gmail.com>
2025-09-28 21:04:01 -0300

    feat: create dataset details page

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from core.views import home, datasets
+from core.views import home, datasets, get_dataset_details
 
 urlpatterns = [
     path('', home, name='home'),
     path('datasets/', datasets, name='datasets'),
+    path('datasets/<str:dataset_name>/', get_dataset_details, name='dataset_details'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .services.datasets_service import DatasetsService
+from django.core.paginator import Paginator
 
 # Create your views here.
 def home(request):
@@ -12,3 +13,16 @@ def datasets(request):
     service = DatasetsService()
     datasets = service.list_datasets()
     return render(request, 'datasets.html', {'datasets': datasets})
+
+def get_dataset_details(request, dataset_name):
+    service = DatasetsService()
+    dataset = service.get_dataset(dataset_name)
+
+    page_number = request.GET.get('page', 1)
+    paginator = Paginator(dataset, 10)  # Show 10 records per page
+    page_obj = paginator.get_page(page_number)
+    
+    return render(request, 'dataset_details.html', {
+        'dataset': dataset_name,
+        'page_obj': page_obj
+    })
