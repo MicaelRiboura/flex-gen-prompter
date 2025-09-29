f9965e79a260d56d27958a80ae3ffc11f6d0f451
MicaelRiboura <micaelribouradossantos@gmail.com>
2025-09-29 09:01:03 -0300

    build: setup env variable of env file

--- flexgenprompter/settings.py
+++ flexgenprompter/settings.py
@@ -10,12 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
