8ddc7ab8541d8da9378787e56f6d4a781c5676ac
MicaelRiboura <micaelribouradossantos@gmail.com>
2025-11-15 16:36:37 -0300

    build(core): create prompt model

--- core/models.py
+++ core/models.py
@@ -11,3 +11,14 @@ class Dataset(models.Model):
     
     def __str__(self):
         return self.name
+    
+class Prompt(models.Model):
+    technique = models.CharField(max_length=255)
+    dataset = models.CharField(max_length=255)
+    node = models.CharField(max_length=255)
+    prompt = models.TextField()
+    
+    class Meta:
+        verbose_name = 'prompt'
+        verbose_name_plural = 'prompts'
+        unique_together = ('technique', 'dataset', 'node')
